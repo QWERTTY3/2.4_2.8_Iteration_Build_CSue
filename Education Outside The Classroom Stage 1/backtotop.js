9e67ab3ea15d2534d6d4c92129742cbe04e8ec60
QWERTTY3 <1csue@tawacollege.school.nz>
2024-08-30 00:19:13 +1200

    Education Outside The Classroom Stage 1

---
+++ Education Outside The Classroom Stage 1/backtotop.js
@@ -0,0 +1,21 @@
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// Controlling the button's scroll 
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the button is clicked, the user is scrolled back to the top of the web page
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;  
+}
